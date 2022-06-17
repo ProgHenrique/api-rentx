c7861548eed49f493b3431f9975d0d2adc6b4975
ProgHenrique <henrique123187@gmail.com>
2022-06-17 12:16:08 -0300

    Cors and Rate Limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata";
 import "dotenv/config";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
 import upload from "@config/upload";
 import { AppError } from "@shared/infra/errors/AppError";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
@@ -17,11 +19,15 @@ createConnection();
 
 const app = express();
 
+app.use(rateLimiter);
+
 app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+
+app.use(cors());
 app.use(router);
 
 app.use(
